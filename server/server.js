d79f8f8141ce3227c96439be69def8242e467c44
Y <nq2019.trghuynhnhuy080202@gmail.com>
2026-04-19 18:04:36 +0700

    fix lai server

--- server/server.js
+++ server/server.js
@@ -3,9 +3,7 @@
   import productRoute from './routes/productRoutes.js';
   import authRoutes from './routes/authRoutes.js';
   import userRoutes from './routes/userRoutes.js';
-  import multer from 'multer';
-  import path from "path"
-  
+
   const app = express();
   const PORT = process.env.PORT || 3000;
   
@@ -14,8 +12,7 @@
   app.use(cors());
 
   app.use(cors({
-    origin: ['https://sang-t4-nhom-11.vercel.app',
-    "http://localhost:5173"],
+    origin: 'https://sang-t4-nhom-11.vercel.app', 
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization']
@@ -31,17 +28,3 @@
   app.listen(PORT, () => {
       console.log(`[SERVER] Server is running on http://localhost:${PORT}`);
   });
-  
-  // Cấu hình multer
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "upload/"); // đúng với thư mục bạn đã tạo
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + "-" + file.originalname);
-  }
-});
-export const upload = multer({ storage });
-
-// Cho phép truy cập ảnh qua URL /upload
-app.use("/upload", express.static(path.join(process.cwd(), "upload")));
